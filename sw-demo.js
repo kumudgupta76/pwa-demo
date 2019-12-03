8655e2135e1f7a28820ed810ec1641c1201dda81
Kumud Gupta <kumudgupta@Kumuds-MacBook-Air.local>
2019-12-03 12:16:06 +0530

    ADD offline page

--- sw-demo.js
+++ sw-demo.js
@@ -1,2 +1,12 @@
-self.addEventListener('install',() => console.log("hi"));
-self.addEventListener('fetch',() => console.log("fetch"));
+self.addEventListener("install", event =>
+  event.waitUntil(caches.open("v1").then(cache => cache.add("/offline.html")))
+);
+
+self.addEventListener("fetch", event => 
+    event.respondWith(
+        fetch(event.request).catch(() => caches.open('v1').then(
+            cache => cache.match('/offline.html')
+            )
+        )
+    )
+);
